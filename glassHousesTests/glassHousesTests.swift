eb97ecb060595eb15ac9280c2607a6df22a3c406
sudolibre <optout.breaker@gmail.com>
2017-05-10 16:11:29 -0400

    using in memory core data store and switching to new constructor methods

--- glassHousesTests/glassHousesTests.swift
+++ glassHousesTests/glassHousesTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import CoreData
 @testable import glassHouses
 
 class glassHousesTests: XCTestCase {
@@ -16,7 +17,17 @@ class glassHousesTests: XCTestCase {
         guard let url = Bundle(for: type(of: self)).url(forResource: "articlesJSON", withExtension: nil, subdirectory: "JSON") else {
             fatalError("articleJSON not found")
         }
-
+        let persistentContainer: NSPersistentContainer = {
+            let persistentContainer = NSPersistentContainer(name: "glassHouses")
+            let memoryDescription = NSPersistentStoreDescription()
+            memoryDescription.type = NSInMemoryStoreType
+            persistentContainer.persistentStoreDescriptions = [memoryDescription]
+            persistentContainer.loadPersistentStores(completionHandler: { (description, error) in
+                if let error = error { print(error) }
+            })
+            return persistentContainer
+        }()
+        let legislator = Legislator(context: persistentContainer.viewContext)
         let jsonData = try? Data(contentsOf: url)
         XCTAssertNotNil(jsonData, "failed to create Data from json file")
         let jsonObject = try? JSONSerialization.jsonObject(with: jsonData!, options: [])
@@ -25,7 +36,7 @@ class glassHousesTests: XCTestCase {
         XCTAssertNotNil(topLevelDictionary, "failed to cast the top level json object from Any to a to dictionary")
         let jsonDictionaries = topLevelDictionary!["value"] as? [[String: Any]]
         XCTAssertNotNil(jsonDictionaries, "failed to cast the json object from Any to an array of dictionaries")
-        let allNewsArticles = jsonDictionaries!.flatMap({NewsArticle.init(json: $0)})
+        let allNewsArticles = jsonDictionaries!.flatMap({Article.fromJSON($0, legislator: legislator, into: persistentContainer.viewContext)})
         XCTAssertTrue(allNewsArticles.count == allNewsArticles.count, "failed to create all News Articles from all json dictionaries")
     }
     
@@ -36,14 +47,23 @@ class glassHousesTests: XCTestCase {
             XCTFail("urls for test legislation could not be found")
             return
         }
-        
+        let persistentContainer: NSPersistentContainer = {
+            let persistentContainer = NSPersistentContainer(name: "glassHouses")
+            let memoryDescription = NSPersistentStoreDescription()
+            memoryDescription.type = NSInMemoryStoreType
+            persistentContainer.persistentStoreDescriptions = [memoryDescription]
+            persistentContainer.loadPersistentStores(completionHandler: { (description, error) in
+                if let error = error { print(error) }
+            })
+            return persistentContainer
+        }()
         let allJSONData = urls.flatMap({try? Data(contentsOf: $0)})
         XCTAssertTrue(urls.count == allJSONData.count, "failed to create Data from all json files")
         let allJSONAny = allJSONData.flatMap({try? JSONSerialization.jsonObject(with: $0, options: [])})
         XCTAssertTrue(urls.count == allJSONAny.count, "failed to create foundation objects from all json data")
         let allJSONDict = allJSONAny.flatMap({$0 as? [String: Any]})
         XCTAssertTrue(urls.count == allJSONDict.count, "failed to cast all json objects from any to dictionary")
-        let allLegislation = allJSONDict.flatMap({Legislation.init(json: $0)})
+        let allLegislation = allJSONDict.flatMap({Legislation.fromJSON($0, into: persistentContainer.viewContext)})
         XCTAssertTrue(urls.count == allLegislation.count, "failed to create Legislation from all json dictionaries")
     }
     
@@ -53,15 +73,24 @@ class glassHousesTests: XCTestCase {
             XCTFail("urls for test legislators could not be found")
             return
         }
-        
+        let persistentContainer: NSPersistentContainer = {
+            let persistentContainer = NSPersistentContainer(name: "glassHouses")
+            let memoryDescription = NSPersistentStoreDescription()
+            memoryDescription.type = NSInMemoryStoreType
+            persistentContainer.persistentStoreDescriptions = [memoryDescription]
+            persistentContainer.loadPersistentStores(completionHandler: { (description, error) in
+                if let error = error { print(error) }
+            })
+            return persistentContainer
+        }()
         let allJSONData = urls.flatMap({try? Data(contentsOf: $0)})
         XCTAssertTrue(urls.count == allJSONData.count, "failed to create Data from all json files")
         let allJSONAny = allJSONData.flatMap({try? JSONSerialization.jsonObject(with: $0, options: [])})
         XCTAssertTrue(urls.count == allJSONAny.count, "failed to create foundation objects from all json data")
         let allJSONDict = allJSONAny.flatMap({$0 as? [String: Any]})
         XCTAssertTrue(urls.count == allJSONDict.count, "failed to cast all json objects from any to dictionary")
-        let allLegislators = allJSONDict.flatMap({Legislator.init(json: $0)})
+        let allLegislators = allJSONDict.flatMap({Legislator.fromJSON($0, into: persistentContainer.viewContext)})
         XCTAssertTrue(urls.count == allLegislators.count, "failed to create legislators from all json dictionaries")
     }
-
+    
 }
